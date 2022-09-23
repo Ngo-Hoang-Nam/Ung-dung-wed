328966e0e63418319eedb6134ec86eec5b405e82
Ngo Hoang Nam <namb1900243@example.com>
2022-09-23 14:32:34 +0700

    Cai dat xu ly loi

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const contacts = require("../controllers/contact.controller");
+const contact = require("../controllers/contact.controller");
 
 const router = express.Router();
 
 router.route("/")
-    .get(contacts.findAll)
-    .post(contacts.create)
-    .deLete(contacts.deleteAll);
+    .get(contact.findAll)
+    .post(contact.create)
+    .delete(contact.deleteAll);
 
 router.route("/favorite")
-    .get(contacts.findAllFavorite);
+    .get(contact.findAllFavorite);
 
 router.route("/:id")
-    .get(contacts.findOne)
-    .put(contacts.update)
-    .deLete(contacts.delete);
+    .get(contact.findOne)
+    .put(contact.update)
+    .delete(contact.delete);
 
 module.exports = router;
